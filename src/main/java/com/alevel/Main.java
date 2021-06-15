fae6fcba617e789c8fd467dcebc3b2df79a6aa6c
Nikita658 <nikita14bykov@gmail.com>
2021-06-15 19:27:22 +0300

    working HW13

--- src/main/java/com/alevel/Main.java
+++ src/main/java/com/alevel/Main.java
@@ -4,23 +4,30 @@ import com.alevel.model.DriverLicence;
 import com.alevel.model.Person;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) {
         final DriverLicence driverLicence = DriverLicence.builder()
-                                                         .categories(new String[]{"A", "B", "C"})
-                                                         .expireDate(Instant.parse("2020-11-30T18:35:24.00Z"))
-                                                         .build();
+                .categories(new String[]{"A", "B", "C"})
+                .expireDate(Instant.parse("2021-11-30T18:35:24.00Z"))
+                .build();
         final Person person = Person.builder()
-                                    .firstName("John")
-                                    .lastName("Smith")
-                                    .driverLicence(Optional.ofNullable(driverLicence))
-                                    .build();
+                .firstName("John")
+                .lastName("Smith")
+                .driverLicence(Optional.ofNullable(driverLicence))
+                .build();
 
+        Instant currentDate = Instant.now();
         Optional.ofNullable(person)
-                // you code here
-        ;
+                .flatMap(Person::getDriverLicence)
+                .filter(driverLicence1 -> currentDate.isBefore(driverLicence1.getExpireDate()))
+                .map(DriverLicence::getCategories)
+                .ifPresentOrElse(categories -> System.out.println(Arrays.toString(categories)),
+                        () -> {
+                            throw new RuntimeException("Driver license is not valid");
+                        });
     }
 }
